f4fbe5acf70e4fcd63fcca6c6d24ba0b70726b93
Guilherme R. <guilhermerebrito@gmail.com>
2025-01-31 15:54:11 -0300

    Modularizando o código

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import leiafloat, leiaint
+
+i = leiaint('Digite um numero Inteiro: ')
+r = leiafloat('Digite um numero Real: ')
+
+print('-'*40)
+print(f'O valor Inteiro digitado foi {i} e o Real foi {r}.')
+print('-'*40)
--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def leiaint(msg):
         except(ValueError, TypeError):
             print('\033[31mERRO! Digite um número Inteiro valido.\033[m')
         except KeyboardInterrupt:
-            print('\033[31mO usuario preferio não informar os dados.\033[m')
+            print('\033[31mO usuário preferiu não informar os dados.\033[m')
             return 0
         else:
             return int(n) 
@@ -22,13 +22,7 @@ def leiafloat(msg):
         except(ValueError, TypeError):
             print('\033[31mERRO! Digite um número Real valido.\033[m')
         except KeyboardInterrupt:
-            print('\033[31mO usuario preferio não informar os dados.\033[m ')
+            print('\033[31mO usuário preferiu não informar os dados.\033[m ')
             return 0
         else:
             return float(n)
-
-
-i = leiaint('Digite um numero Inteiro: ')
-r = leiafloat('Digite um numero Real: ')
-
-print(f'O valor Inteiro digitado foi {i} e o Real foi {r}')
